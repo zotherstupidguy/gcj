756e7b927e075375e994135c6f48f39c139c0abd
zotherstupidguy <zotherstupidguy@gmail.com>
2015-05-04 09:18:23 +0300

    Add solution.c for low level attempt to build a datastructure tree of the langauge

---
+++ alien_language/solution.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct tree_el {
+  int val;
+  struct tree_el * right, * left;
+};
+
+typedef struct tree_el node;
+
+void insert(node ** tree, node * item) {
+  if(!(*tree)) {
+    *tree = item;
+    return;
+  }
+  if(item ->val<(*tree) -> val)
+    insert(&(*tree)->left, item);
+  else if(item->val>(*tree)->val)
+    insert(&(*tree)->right, item);
+};
+
+void printout(node * tree) {
+if (tree -> left) printout (tree -> left);
+printf(
+}
